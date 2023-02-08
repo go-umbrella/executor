c34d46bd014bfcd85428bee55d50163872f397f6
Leonardo Carmona <leocarmona_@outlook.com>
2023-02-08 16:22:44 -0300

    feat: initializing executor workers

--- executor.go
+++ executor.go
@@ -1,13 +1,74 @@
 package executor
 
+import (
+	"runtime"
+	"sync"
+	"sync/atomic"
+)
+
 type (
 	Executor interface {
 	}
 
 	executor struct {
+		config             Config
+		taskQueue          chan func()
+		workerWG           sync.WaitGroup
+		workerStopSignal   chan struct{}
+		workerCount        uint64
+		workerRunningCount uint64
 	}
 )
 
-func New() Executor {
-	return new(executor)
+func New(config Config) Executor {
+	config.normalize()
+	return (&executor{
+		config:           config,
+		taskQueue:        make(chan func(), config.QueueSize),
+		workerStopSignal: make(chan struct{}, runtime.NumCPU()),
+	}).initialize()
+}
+
+func (e *executor) initialize() *executor {
+	e.initializeWorkers()
+	return e
+}
+
+func (e *executor) initializeWorkers() {
+	if !e.config.EagerInitialization {
+		return
+	}
+
+	for i := uint64(0); i < e.config.Concurrency; i++ {
+		e.newWorker()
+	}
+}
+
+func (e *executor) newWorker() bool {
+	if atomic.LoadUint64(&e.workerCount) >= e.config.Concurrency {
+		return false
+	}
+
+	e.workerWG.Add(1)
+	atomic.AddUint64(&e.workerCount, 1)
+
+	go func() {
+		defer func() {
+			atomic.AddUint64(&e.workerCount, ^uint64(0))
+			e.workerWG.Done()
+		}()
+
+		for {
+			select {
+			case task := <-e.taskQueue:
+				atomic.AddUint64(&e.workerRunningCount, 1)
+				task()
+				atomic.AddUint64(&e.workerRunningCount, ^uint64(0))
+			case <-e.workerStopSignal:
+				return
+			}
+		}
+	}()
+
+	return true
 }
--- executor_test.go
+++ executor_test.go
@@ -2,23 +2,88 @@ package executor
 
 import (
 	"github.com/stretchr/testify/assert"
+	"runtime"
+	"sync/atomic"
 	"testing"
 )
 
 func TestNew(t *testing.T) {
+	type expected struct {
+		config             Config
+		workerCount        uint64
+		workerRunningCount uint64
+	}
+
 	testCases := []struct {
-		name          string
-		expectedValue Executor
+		name     string
+		executor Executor
+		expected expected
 	}{
 		{
-			name:          "should_return_new_executor",
-			expectedValue: new(executor),
+			name: "should_eager_initialize_workers",
+			executor: New(Config{
+				Name:                "my-beautiful-executor",
+				Concurrency:         4,
+				QueueSize:           16,
+				EagerInitialization: true,
+			}),
+			expected: expected{
+				config: Config{
+					Name:                "my-beautiful-executor",
+					Concurrency:         4,
+					QueueSize:           16,
+					EagerInitialization: true,
+				},
+				workerCount:        4,
+				workerRunningCount: 0,
+			},
+		},
+		{
+			name: "should_eager_initialize_default_workers",
+			executor: New(Config{
+				Name:                "my-beautiful-executor",
+				Concurrency:         0,
+				QueueSize:           16,
+				EagerInitialization: true,
+			}),
+			expected: expected{
+				config: Config{
+					Name:                "my-beautiful-executor",
+					Concurrency:         uint64(runtime.NumCPU()),
+					QueueSize:           16,
+					EagerInitialization: true,
+				},
+				workerCount:        uint64(runtime.NumCPU()),
+				workerRunningCount: 0,
+			},
+		},
+		{
+			name: "should_lazily_initialize_workers",
+			executor: New(Config{
+				Name:                "my-beautiful-executor",
+				Concurrency:         4,
+				QueueSize:           16,
+				EagerInitialization: false,
+			}),
+			expected: expected{
+				config: Config{
+					Name:                "my-beautiful-executor",
+					Concurrency:         4,
+					QueueSize:           16,
+					EagerInitialization: false,
+				},
+				workerCount:        0,
+				workerRunningCount: 0,
+			},
 		},
 	}
 
 	for _, testCase := range testCases {
 		t.Run(testCase.name, func(t *testing.T) {
-			assert.Equal(t, testCase.expectedValue, New())
+			executor := testCase.executor.(*executor)
+			assert.Equal(t, testCase.expected.config, executor.config)
+			assert.Equal(t, testCase.expected.workerCount, atomic.LoadUint64(&executor.workerCount))
+			assert.Equal(t, testCase.expected.workerRunningCount, atomic.LoadUint64(&executor.workerRunningCount))
 		})
 	}
 }
