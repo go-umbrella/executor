0d8487c73d0ee364e9076679e08515d801737153
Leonardo Carmona <leocarmona_@outlook.com>
2023-02-13 14:58:59 -0300

    feat: adding task context

---
+++ taskcontext.go
@@ -0,0 +1,45 @@
+package executor
+
+import (
+	"context"
+	"time"
+)
+
+type (
+	TaskContext interface {
+		Args() []interface{}
+		context.Context
+	}
+
+	taskContext struct {
+		ctx  context.Context
+		args []interface{}
+	}
+)
+
+func newTaskContext(ctx context.Context, args []interface{}) *taskContext {
+	return &taskContext{
+		ctx:  ctx,
+		args: args,
+	}
+}
+
+func (c *taskContext) Args() []interface{} {
+	return c.args
+}
+
+func (c *taskContext) Deadline() (deadline time.Time, ok bool) {
+	return c.ctx.Deadline()
+}
+
+func (c *taskContext) Done() <-chan struct{} {
+	return c.ctx.Done()
+}
+
+func (c *taskContext) Err() error {
+	return c.ctx.Err()
+}
+
+func (c *taskContext) Value(key any) any {
+	return c.ctx.Value(key)
+}
